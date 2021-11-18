51a782003e40ae17841cfd2d5f52be7fbcb656ef
Adam <adambokun@gmail.com>
2021-11-18 13:29:07 +0300

    add project

---
+++ ChatWithMe/ChatWithMe/Controller/ConversationsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ChatWithMe
+//
+//  Created by Adam Bokun on 16.11.21.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+
+}
+
